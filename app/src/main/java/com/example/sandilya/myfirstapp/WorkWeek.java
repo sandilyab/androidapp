f1345acbace83cb90742b07ace5cc6e4b02a9454
Sandilya Bhagi <Sandilya Bhagi>
2016-12-30 22:58:37 -0800

    Added current work week info in WorkWeek activivty

--- app/src/main/java/com/example/sandilya/myfirstapp/WorkWeek.java
+++ app/src/main/java/com/example/sandilya/myfirstapp/WorkWeek.java
@@ -32,30 +32,37 @@ public class WorkWeek extends AppCompatActivity {
         setContentView(R.layout.activity_work_week);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //generate current date values
+        Calendar cal_orig = Calendar.getInstance();
+        int week_orig = cal_orig.get(Calendar.WEEK_OF_YEAR);
+        SelectedDateView = (TextView) findViewById(R.id.original_date);
+        SelectedDateView.setText("Current Work Week: " +week_orig+"");
+
+        //handle date picker values
         String s = (getIntent().getStringExtra("Selected Date"));
         String[]strArray= s.split(",");
-        int month = Integer.parseInt(strArray[0]) ;
-        month = month-1;
+
+        int month = Integer.parseInt(strArray[0]) -1 ;
         int date = Integer.parseInt(strArray[1]);
         int year = Integer.parseInt(strArray[2]);
-        Log.w("myApp",strArray[0]);
-        Log.w("myApp",strArray[1]);
-        Log.w("myApp",strArray[2]);
         String format = "yyMMdd";
-        //SimpleDateFormat df = new SimpleDateFormat(format);
-        //Date date2 = df.parse(Arrays.toString(strArray));
+
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.MONTH, month);
         cal.set(Calendar.DATE, date);
         cal.set(Calendar.YEAR, year);
-        Date date2;
-        date2= cal.getTime();
+
+        Date date2= cal.getTime();
         int week = cal.get(Calendar.WEEK_OF_YEAR);
         int year2 = cal.get(Calendar.YEAR);
+
         Log.w("myApp", Integer.toString(week));
         SelectedDateView = (TextView) findViewById(R.id.selected_date);
-        SelectedDateView.setText(week+"");
+        SelectedDateView.setText("Target Work Week: " +week+"");
         Log.w("myApp", "no network");
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
